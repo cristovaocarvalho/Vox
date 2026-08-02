69d269ff23a047fbae2665460364c4d5c10cfa61
cristovaocarvalhorb <cristovaocarvalhorb@gmail.com>
2026-08-02 16:24:52 -0300

    chore: upgrade electron, vite, and build dependencies while initializing output directory structure

--- out/preload/preload.js
+++ out/preload/preload.js
@@ -1,56 +1,53 @@
-"use strict";
 Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-const { contextBridge, ipcRenderer } = require("electron");
-const voxApi = {
-  // Vox Type (Digitação por voz)
-  startRecording: () => ipcRenderer.invoke("vox:start-recording"),
-  stopRecording: (audioData) => ipcRenderer.invoke("vox:stop-recording", audioData),
-  sendAudioChunk: (chunk) => ipcRenderer.send("vox:audio-chunk", chunk),
-  sendAudioLevel: (level) => ipcRenderer.send("vox:audio-level", level),
-  transcribeChunk: (audioData) => ipcRenderer.invoke("vox:transcribe-chunk", audioData),
-  showDock: () => ipcRenderer.invoke("vox:show-dock"),
-  hideDock: () => ipcRenderer.invoke("vox:hide-dock"),
-  minimize: () => ipcRenderer.invoke("vox:minimize"),
-  // Vox Media (Transcrição de mídia)
-  getVideoInfo: (url, cookiesFromBrowser) => ipcRenderer.invoke("vox:get-video-info", url, cookiesFromBrowser),
-  downloadAudio: (url, cookiesFromBrowser) => ipcRenderer.invoke("vox:download-audio", url, cookiesFromBrowser),
-  transcribeMedia: (options) => ipcRenderer.invoke("vox:transcribe-media", options),
-  deleteFile: (filePath) => ipcRenderer.invoke("vox:delete-file", filePath),
-  selectFile: () => ipcRenderer.invoke("vox:select-file"),
-  // Configurações & Banco de Dados
-  getSettings: () => ipcRenderer.invoke("vox:get-settings"),
-  saveSettings: (settings) => ipcRenderer.invoke("vox:save-settings", settings),
-  // Event Listeners
-  onDockTextUpdate: (callback) => {
-    const handler = (_event, text) => callback(text);
-    ipcRenderer.on("vox:dock-text-update", handler);
-    return () => ipcRenderer.removeListener("vox:dock-text-update", handler);
-  },
-  onToggleRecording: (callback) => {
-    const handler = (_event, recording) => callback(recording);
-    ipcRenderer.on("vox:toggle-recording", handler);
-    return () => ipcRenderer.removeListener("vox:toggle-recording", handler);
-  },
-  onVolumeUpdate: (callback) => {
-    const handler = (_event, data) => callback(data);
-    ipcRenderer.on("vox:volume-update", handler);
-    return () => ipcRenderer.removeListener("vox:volume-update", handler);
-  },
-  onTranscriptResult: (callback) => {
-    const handler = (_event, text) => callback(text);
-    ipcRenderer.on("vox:transcript-result", handler);
-    return () => ipcRenderer.removeListener("vox:transcript-result", handler);
-  },
-  onPartialTranscript: (callback) => {
-    const handler = (_event, text) => callback(text);
-    ipcRenderer.on("vox:partial-transcript", handler);
-    return () => ipcRenderer.removeListener("vox:partial-transcript", handler);
-  },
-  onDownloadProgress: (callback) => {
-    const handler = (_event, data) => callback(data);
-    ipcRenderer.on("vox:download-progress", handler);
-    return () => ipcRenderer.removeListener("vox:download-progress", handler);
-  }
+//#region electron/preload.ts
+var { contextBridge, ipcRenderer } = require("electron");
+var voxApi = {
+	startRecording: () => ipcRenderer.invoke("vox:start-recording"),
+	stopRecording: (audioData) => ipcRenderer.invoke("vox:stop-recording", audioData),
+	sendAudioChunk: (chunk) => ipcRenderer.send("vox:audio-chunk", chunk),
+	sendAudioLevel: (level) => ipcRenderer.send("vox:audio-level", level),
+	transcribeChunk: (audioData) => ipcRenderer.invoke("vox:transcribe-chunk", audioData),
+	showDock: () => ipcRenderer.invoke("vox:show-dock"),
+	hideDock: () => ipcRenderer.invoke("vox:hide-dock"),
+	minimize: () => ipcRenderer.invoke("vox:minimize"),
+	getVideoInfo: (url, cookiesFromBrowser) => ipcRenderer.invoke("vox:get-video-info", url, cookiesFromBrowser),
+	downloadAudio: (url, cookiesFromBrowser) => ipcRenderer.invoke("vox:download-audio", url, cookiesFromBrowser),
+	transcribeMedia: (options) => ipcRenderer.invoke("vox:transcribe-media", options),
+	deleteFile: (filePath) => ipcRenderer.invoke("vox:delete-file", filePath),
+	selectFile: () => ipcRenderer.invoke("vox:select-file"),
+	getSettings: () => ipcRenderer.invoke("vox:get-settings"),
+	saveSettings: (settings) => ipcRenderer.invoke("vox:save-settings", settings),
+	onDockTextUpdate: (callback) => {
+		const handler = (_event, text) => callback(text);
+		ipcRenderer.on("vox:dock-text-update", handler);
+		return () => ipcRenderer.removeListener("vox:dock-text-update", handler);
+	},
+	onToggleRecording: (callback) => {
+		const handler = (_event, recording) => callback(recording);
+		ipcRenderer.on("vox:toggle-recording", handler);
+		return () => ipcRenderer.removeListener("vox:toggle-recording", handler);
+	},
+	onVolumeUpdate: (callback) => {
+		const handler = (_event, data) => callback(data);
+		ipcRenderer.on("vox:volume-update", handler);
+		return () => ipcRenderer.removeListener("vox:volume-update", handler);
+	},
+	onTranscriptResult: (callback) => {
+		const handler = (_event, text) => callback(text);
+		ipcRenderer.on("vox:transcript-result", handler);
+		return () => ipcRenderer.removeListener("vox:transcript-result", handler);
+	},
+	onPartialTranscript: (callback) => {
+		const handler = (_event, text) => callback(text);
+		ipcRenderer.on("vox:partial-transcript", handler);
+		return () => ipcRenderer.removeListener("vox:partial-transcript", handler);
+	},
+	onDownloadProgress: (callback) => {
+		const handler = (_event, data) => callback(data);
+		ipcRenderer.on("vox:download-progress", handler);
+		return () => ipcRenderer.removeListener("vox:download-progress", handler);
+	}
 };
 contextBridge.exposeInMainWorld("vox", voxApi);
+//#endregion
 exports.voxApi = voxApi;
